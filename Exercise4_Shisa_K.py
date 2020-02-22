7b88b3889f3f7458f603beeffe665434a201513e
Lhiw <shisak30@gmail.com>
2020-02-22 16:09:29 +0700

    Exercise4
    homework

---
+++ Exercise4_Shisa_K.py
@@ -0,0 +1,10 @@
+FE = "73"
+GB = "85"
+IC = "50"
+CP = "72"
+
+print("-----------Your Score-----------")
+print("Foundation English   :",FE)
+print("General Business     :",GB)
+print("Computer Programming :",CP)
+print("Introduction to Computer Systems :",IC)
